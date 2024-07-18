ca6dbd1976e121ef7d2578a098b5cd29ba3874f1
Selim Köse <selim-kose@live.com>
2024-07-18 08:27:28 +0200

    Made changes in output formating

--- resume_parsing.py
+++ resume_parsing.py
@@ -1,77 +1,67 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[36]:
 
 
-import spacy
-import pdfminer
-import re
-import os
-import pandas as pd
+import spacy #nlp
+import pdfminer #pdf2tx
+import re #regex
+import os #file manip
+import pandas as pd #csv - tabular
 
 
-# In[2]:
+# In[37]:
 
 
 import pdf2txt
 
 
-# In[3]:
+# # converting pdf to txt
+
+# In[39]:
 
 
 def convert_pdf(f):
 
-    output_filename = os.path.basename(os.path.splitext(f)[0]) + '.txt'
-    #output_filepath = os.path.join('..', 'data', 'output', output_filename)
-    output_filepath = os.path.join('output/txt/', output_filename)
-    #logging.info('Writing text from {} to {}'.format(f, output_filepath))
-    pdf2txt.main(args=[f, '--outfile', output_filepath])
+    output_filename = os.path.basename(os.path.splitext(f)[0]) + ".txt"
+    output_filepath = os.path.join("output/txt/", output_filename)
+    pdf2txt.main(args=[f, "--outfile", output_filepath]) #pdf to txt and save it in the given location
     print(output_filepath + " saved successfully!!!")
     return open(output_filepath).read()
 
 
-# In[4]:
+# # load the language model
 
-
-os.path.splitext("Sally CrookCV.pdf")[0]
-
-
-# In[5]:
+# In[41]:
 
 
 nlp = spacy.load("en_core_web_sm")
 
 
-# In[6]:
+# In[43]:
 
 
-#phone_num credit https://stackoverflow.com/a/3868861
-
-
-# In[7]:
-
-
-result_dict = {'name': [], 'phone': [], 'email': [], 'skills': []}
+result_dict = {'name': [], 'phone': [], 'email': [], 'skills': []} 
 names = []
 phones = []
 emails = []
 skills = []
 
 
-# In[8]:
+# In[44]:
 
 
 def parse_content(text):
-    skillset = re.compile('python|java|sql|hadoop|tableau')
-    phone_num = re.compile('(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    skillset = re.compile("python|java|sql|hadoop|tableau")
+    phone_num = re.compile(r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}')
     doc = nlp(text)
-    name = [entity.text for entity in doc.ents if entity.label_ is 'PERSON'][0]
+    name = [entity.text for entity in doc.ents if entity.label_ == "PERSON"][0].strip()
     print(name)
     email = [word for word in doc if word.like_email == True][0]
     print(email)
-    phone = str(re.findall(phone_num,text.lower()))
-    skills_list = re.findall(skillset,text.lower())
+    phone = str(re.findall(phone_num, text.lower()))
+    skills_list = re.findall(skillset, text.lower())
     unique_skills_list = str(set(skills_list))
     names.append(name)
     emails.append(email)
@@ -80,7 +70,7 @@ def parse_content(text):
     print("Extraction completed successfully!!!")
 
 
-# In[9]:
+# In[45]:
 
 
 for file in os.listdir('resumes/'):
@@ -90,24 +80,24 @@ for file in os.listdir('resumes/'):
         parse_content(txt)
 
 
-# In[10]:
+# In[46]:
 
 
 result_dict['name'] = names
 result_dict['phone'] = phones
 result_dict['email'] = emails
 result_dict['skills'] = skills
-#print(result_dict)
+print(result_dict)
 
 
-# In[11]:
+# In[47]:
 
 
 result_df = pd.DataFrame(result_dict)
 result_df
 
 
-# In[12]:
+# In[35]:
 
 
 result_df.to_csv('output/csv/parsed_resumes.csv')
